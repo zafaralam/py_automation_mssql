3ba13585f1cbeabdd5aea7cb25f52140634e81ca
Zaf <fastalam420@gmail.com>
2020-03-22 19:49:02 +1100

    bug fix

--- comman.py
+++ comman.py
@@ -9,11 +9,11 @@ def get_list_of_avaiable_queries():
     query_files = {}
     for path, dirs, files in os.walk(QUERY_PATH):
         for num, filename in enumerate(files):
-            query_files[num] = filename
+            query_files[num+1] = filename
 
     return query_files
 
 
 def print_list_of_queries():
     for key, val in get_list_of_avaiable_queries().items():
-        print("\t{0}) {1}".format(key+1, val))
+        print("\t{0}) {1}".format(key, val))
