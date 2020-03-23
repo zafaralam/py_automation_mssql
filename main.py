fc4031e0d3229671277dabafec4ae838dad709fb
Zaf <fastalam420@gmail.com>
2020-03-23 21:43:28 +1100

    bug fix for query execution

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ def write_data_to_file(script_name, filename, export_type):
         with open(os.path.join(QUERY_PATH, script_name), 'r') as file:
             logger.info("Reading Script %s", script_name)
             query = file.read()
+            query = "SET NOCOUNT ON;\n{0}\nSET NOCOUNT OFF;".format(query)
             with pyodbc.connect(CONN_STR) as cnxn:
                 logger.info("Running query \n %s", query)
                 tmp_df = pd.read_sql(query, cnxn)
@@ -63,10 +64,10 @@ def write_data_to_file(script_name, filename, export_type):
                     pass
         print("Your exported file is: ", full_file_path)
     except Exception as exception:
-        logger.exception("Error Saving file")
+        logger.exception("Error Saving file", exc_info=True)
 
 
-if __name__ == "__main__":
+def program():
     setup()
     available_queries = get_list_of_avaiable_queries()
     if len(available_queries.items()) == 0:
@@ -92,10 +93,10 @@ run the program.
             print("\n")
 
             if user_input < 0 or user_input > 1:
-                print(
-                    "Invalid selection, please select from one of the available options.")
+                raise Exception(
+                    "Invalid main menu selection {0}".format(user_input))
             elif user_input == 0:
-                print("Bye :), see you next time.")
+                print("Bye :) see you next time.")
                 _exit = True
                 exit(0)
             else:
@@ -105,7 +106,7 @@ run the program.
                 while _in_query_selection:
                     print("List of avaiable queries")
                     print_list_of_queries()
-                    print("\t0) Back")
+                    print("\t0) <-- Back")
 
                     user_query_selection = input("Please select a query: ")
 
@@ -113,26 +114,30 @@ run the program.
                         user_query_selection = int(user_query_selection)
 
                         if user_query_selection < 0 or user_query_selection > len(available_queries.items()):
-                            print("Invalid selection, please try again.")
+                            # print(", please try again.")
+                            raise Exception(
+                                "Invalid query selection {0}".format(user_query_selection))
                         elif user_query_selection == 0:
                             _in_query_selection = False
                         else:
                             _in_query_selection = False
                             query_selected = True
                     except Exception as file_exception:
-                        print("Invalid selection, please try again.")
+                        logger.exception(
+                            "Error selecting query", exc_info=True)
                 if query_selected:
                     _in_export_type_selection = True
                     filename = ""
                     while _in_export_type_selection:
                         print("\nExport types avaiable")
-                        print("\t1. Excel\n\t2. CSV\n\t0. Back")
+                        print("\t1. Excel\n\t2. CSV\n\t0. <-- Back")
                         export_type = input(
                             "Please select export type: ")
                         try:
                             export_type = int(export_type)
                             if export_type < 0 or export_type > 2:
-                                print("Invalid selection, please try again")
+                                raise Exception(
+                                    "Invalid export type selection {0}".format(A_EXPORT_TYPES[export_type]))
                             elif export_type == 0:
                                 _in_export_type_selection = False
                             elif export_type >= 1 and export_type <= 2:
@@ -143,13 +148,17 @@ run the program.
                             else:
                                 _in_export_type_selection = False
                         except Exception as export_exception:
-                            print("Invalid selection, please try again")
+                            logger.exception(
+                                "Error selecting export type", exc_info=True)
 
                     if export_type in A_EXPORT_TYPES:
                         write_data_to_file(
                             available_queries[user_query_selection], filename, export_type)
 
         except Exception as exception:
-            print("Invalid selection, please select from one of the available options.")
+            logger.exception(
+                "Error selecting main menu", exc_info=True)
 
-    # print_list_of_queries()
+
+if __name__ == "__main__":
+    program()
